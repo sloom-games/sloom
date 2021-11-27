13ffa015e7c92fb200e7ed7dc438c3db6c961a3b
zLennii <87238895+zLennii@users.noreply.github.com>
2021-11-27 13:21:03 +0000

    f1 2011 turkey

--- sloom.cs
+++ sloom.cs
@@ -1,1 +1,6 @@
-real
+private void Form1_Load(object sender, EventArgs e)
+{
+    PictureBox pb1 = new PictureBox();            
+    pb1.ImageLocation = "";
+    pb1.SizeMode = PictureBoxSizeMode.AutoSize;
+}
