1d4cc924040e32a4eabfa819a773519abb84d514
zLennii <87238895+zLennii@users.noreply.github.com>
2021-11-27 23:04:28 +0000

    Delete sloom.cs

--- sloom.cs
+++
@@ -1,6 +0,0 @@
-private void Form1_Load(object sender, EventArgs e)
-{
-    PictureBox pb1 = new PictureBox();            
-    pb1.ImageLocation = "";
-    pb1.SizeMode = PictureBoxSizeMode.AutoSize;
-}
