f63545873bacef0fb9ec836db75a2bdca66e9130
Optimal <80202462+Optimal7@users.noreply.github.com>
2022-01-04 18:13:55 +0200

    Update

--- sloom.py
+++ sloom.py
@@ -6,6 +6,7 @@ import sys
 import os
 import time
 from pypresence import Presence
+import datetime
 
 #Pyinstaller
 def resource_path(relative_path):
@@ -25,10 +26,20 @@ root.resizable(width=False, height=False)
 height = 300
 width = 300
 
+global elapsed
+elapsed = time.time()
+
+#Time Elapsed
+def time_elapsed():
+    session = str(datetime.timedelta(seconds=int(round(time.time()-elapsed))))
+    timer.config(text=session)
+    timer.after(200, time_elapsed)
+
+#Sloom Music
 pygame.mixer.init()
 def sloom_music():
     pygame.mixer.music.load("Sloom.mp3")
-    pygame.mixer.music.play(loops=1)
+    pygame.mixer.music.play(loops=0)
 
 #Discord Presence
 #if discord is not open it wont crash the program
@@ -45,13 +56,23 @@ except:
 sloom_music()
 
 #UI
-sloom = Label(root, text="Sloom", font=("Comic Sans MS", 16))
+sloom = Label(root, text="Sloom", font=("Comic Sans MS", 35, "bold"))
 sloom.pack()
 
+#SLOOM IMAGE !
 image = PhotoImage(file=r"sloom.png")
 canvas = Canvas(root, height=height, width=width)
 canvas.create_image(width/2, height/2, anchor="center", image=image)
 canvas.pack()
+
+#Time Wasted Text
+time_wasted = Label(root, text="Time Wasted", font=("Comic Sans MS", 15, "bold"))
+time_wasted.pack()
+
+#Timer
+timer = Label(root, font=("Comic Sans MS", 20))
+timer.pack()
+time_elapsed()
 
 #this
 root.mainloop()
